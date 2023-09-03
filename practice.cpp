0aab9b7cafa6a354f030fd2999661bdd5ae91463
Oleg <oleg25111@inbox.ru>
2023-09-03 09:13:40 +0300

    Lesson 1.2.3 Practice 2

--- practice.cpp
+++ practice.cpp
@@ -1,15 +1,23 @@
 #include <iostream>
+#include <string>
 #include <vector>
 #include <algorithm>
 
 using namespace std;
 
-void PrintWorryCount(const vector<bool>& is_nervous) {
-    cout << count(is_nervous.begin(), is_nervous.end(), true);
+bool CheckFunc (const string str) {
+    return !str.empty() && (str[0] == "A" || str[0] == "a");
+}
+
+int CountStartsWithA(const vector<string>& xs) {
+    return count_if(xs.begin(), xs.end(), CheckFunc);
 }
 
 int main() {
-    // не меняйте содержимое функции main
-    PrintWorryCount({true, true, false, true});
+    // не меняйте тело main
+    cout << CountStartsWithA({"And"s, "another"s, "one"s, "gone"s, "another"s,
+                              "one"s
+                              "bites"s,
+                              "the"s, "dust"s});
     return 0;
 }
